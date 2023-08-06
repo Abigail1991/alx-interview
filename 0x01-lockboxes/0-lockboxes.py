31ddcf59259213915aacf480751d3854346bf676
Abigail Acheampomaa Opoku <acheapomaa.abigail.opoku@gmail.com>
2023-08-06 18:09:20 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lockboxes module.
+
+This module contains a function related to the lockboxes.
+
+"""
+
+
+def canUnlockAll(boxes):
+    n = len(boxes)  # Total number of boxes
+    visited = [False] * n  # Keep track of visited boxes
+    visited[0] = True  # The first box is unlocked initially
+    stack = [0]  # Start with the first box in the stack
+    
+    while stack:
+        current_box = stack.pop()
+        
+        # Iterate through keys in the current box
+        for key in boxes[current_box]:
+            if key >= 0 and key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+    
+    # Check if all boxes have been visited
+    return all(visited)
+
