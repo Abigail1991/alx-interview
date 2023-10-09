9db88fed23de5449951ccb5fa698f5c201b377a0
Abigail Acheampomaa Opoku <acheapomaa.abigail.opoku@gmail.com>
2023-10-09 16:05:39 +0000

    tasks

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,46 +1,31 @@
 #!/usr/bin/python3
-def my_function():
-"""
-This is a docstring that provides information about my_function.
-"""
+""" Module for Prime Game """
 
-def is_winner(x, nums):
-"""
-This is a docstring that provides information about the is_winner function.
-    
-Args:
-    x (int): The number of rounds.
-    nums (list): An array of n for each round.
-        
-Returns:
-    str: The name of the player that won the most rounds or None if the winner cannot be determined.
-"""
-    def sieve_of_eratosthenes(limit):
-        primes = [True] * (limit + 1)
-        primes[0] = primes[1] = False
-        p = 2
-        while p * p <= limit:
-            if primes[p]:
-                for i in range(p * p, limit + 1, p):
-                    primes[i] = False
-            p += 1
-        return [i for i, is_prime in enumerate(primes) if is_prime]
 
-    def game_winner(n):
-        primes = sieve_of_eratosthenes(n)
-        num_primes = len(primes)
-        if num_primes % 2 == 0:
-            return "Ben"
-        else:
-            return "Maria"
-
-    winners = [game_winner(n) for n in nums]
-    maria_wins = winners.count("Maria")
-    ben_wins = winners.count("Ben")
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+def isWinner(x, nums):
+    """Solves Prime Game"""
+    if not nums or x < 1:
         return None
+    n = max(nums)
+    sieve = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not sieve[i]:
+            continue
+        for j in range(i*i, n + 1, i):
+            sieve[j] = False
+
+    sieve[0] = sieve[1] = False
+    c = 0
+    for i in range(len(sieve)):
+        if sieve[i]:
+            c += 1
+        sieve[i] = c
+
+    player1 = 0
+    for n in nums:
+        player1 += sieve[n] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
